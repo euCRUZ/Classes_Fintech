1de8b8590f03837fc69351c835f9fbb79b3028fd
Giovanni <giovannicruzvinhedo@gmail.com>
2024-03-04 17:04:29 -0300

    #

--- src/br/com/classes_fintech/Entidades/Despesa.java
+++ src/br/com/classes_fintech/Entidades/Despesa.java
@@ -1,9 +1,9 @@
 package br.com.classes_fintech.Entidades;
 
-public class Despesa extends Transacao{
+//public class Despesa extends Transacao{
 
 
-    public Despesa(String categoria, Double valor) {
-        super(categoria, valor);
-    }
+//    public Despesa(String categoria, Double valor) {
+//        super(categoria, valor);
+//    }
 }
